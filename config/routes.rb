d32289210b531b9725d87af6de8691b7df44712f
vagrant <vagrant@precise32.(none)>
2017-02-08 22:41:11 +0000

    Gems, Starting Layout, Starting Lotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'lotes/new'
+
+  get 'lotes/show'
+
+  get 'lotes/index'
+
   get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

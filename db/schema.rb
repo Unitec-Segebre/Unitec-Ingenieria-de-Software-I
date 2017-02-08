b002fab0148710a4174e92476b1053429e59cd2c
AstroThing <andres.rodriguezhn@gmail.com>
2017-02-08 04:01:28 -0600

    Generated Project model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207202036) do
+ActiveRecord::Schema.define(version: 20170208095917) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"

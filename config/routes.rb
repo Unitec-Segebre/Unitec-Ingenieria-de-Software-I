14809b427b9374a8fc5753aacf29a3a35d788b16
AstroThing <andres.rodriguezhn@gmail.com>
2017-02-09 12:36:26 -0600

    Fixed project form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'sessions#new'
 
-  resources :projects, only: [:index, :new, :show] do
+  resources :projects, only: [:index, :new, :create, :show] do
     resources :lots, only: :new
   end
 

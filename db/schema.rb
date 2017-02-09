94af8c0e5c518eb40b6a32d56a6baa8986fdc070
AstroThing <andres.rodriguezhn@gmail.com>
2017-02-09 01:27:59 -0600

    Added image_url column to projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209064253) do
+ActiveRecord::Schema.define(version: 20170209072541) do
 
   create_table "lots", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170209064253) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "manager"
+    t.string   "image_url"
   end
 
   create_table "users", force: :cascade do |t|

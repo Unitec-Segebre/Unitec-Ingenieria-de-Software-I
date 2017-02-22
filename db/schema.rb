e995ec6eeb451f8135b6dc4c50504995b6d0cb2d
AstroThing <andres.rodriguezhn@gmail.com>
2017-02-22 09:33:41 -0600

    Generated Variable model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220231105) do
+ActiveRecord::Schema.define(version: 20170222152730) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,16 @@ ActiveRecord::Schema.define(version: 20170220231105) do
     t.string   "last_name"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "variables", force: :cascade do |t|
+    t.string   "measurement_unit"
+    t.decimal  "unit_cost"
+    t.string   "name"
+    t.integer  "category_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["category_id"], name: "index_variables_on_category_id"
   end
 
 end

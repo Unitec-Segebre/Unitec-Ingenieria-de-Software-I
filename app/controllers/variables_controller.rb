43f5bd19dc7df4a6351e4f8ed59be2fdbece9a7c
Juan Enrique Segebre Zaghmout <juansegebre@gmail.com>
2017-02-23 03:05:54 -0600

    solo se puede poner 2 puntos decimales

--- app/controllers/variables_controller.rb
+++ app/controllers/variables_controller.rb
@@ -3,4 +3,28 @@ class VariablesController < ApplicationController
     @variables = Variable.all
     @variable = Variable.new
   end
+
+  def create
+    @variable = Variable.new(variable_params)
+
+    if @variable.save
+      redirect_to variables_path, notice: "Variable #{@variable.name} creada exitosamente!"
+    else
+      flash[:error] = "Un error ha ocurrido al crear una categoria."
+      render :index
+    end
+  end
+
+  def destroy
+    @variable = Variable.find(params[:id])
+
+    @variable.destroy
+      redirect_to variables_path, notice: "Variable #{@variable.name} eliminada correctamente!"
+  end
+
+  protected
+    def variable_params
+      params.require(:variable).permit(:name, :category_id, :measurement_unit, :unit_cost)
+    end
+
 end

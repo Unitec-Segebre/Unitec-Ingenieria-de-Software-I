7548113088cfa605f5faa0eb6553ff41ad90afe5
Juan Enrique Segebre Zaghmout <juansegebre@gmail.com>
2017-03-07 16:14:34 -0600

    changed uniqueness validation in models\users.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, :first_name, :last_name, presence: true
-  validates :email, uniqueness: true
+  validates :username, uniqueness: true
 
 end

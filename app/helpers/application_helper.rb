75cbdf4950be084e6c3a971550f981c45cf5eae4
Kamil <kamil@unitec.edu>
2017-03-08 17:43:14 -0600

    Removed new_button shared partial and replaced with helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
 		return nil unless date
 		date.strftime("%d / %m / %Y")
 	end
+
+	def button(type, icon, string)
+		button_tag(type: 'button', class: "btn #{type} btn-outline btn-lg", data: {toggle: 'modal', target: '#myModal'}, onClick:"focus_field('#field1')", style: 'margin-bottom:20px') do
+			content_tag(:span, "", class: "fa #{icon}") +
+			" " +
+			string
+		end
+	end
 end

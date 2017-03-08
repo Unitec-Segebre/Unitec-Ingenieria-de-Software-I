49bce3a78b5ead117b9f67d69e9e6771889e1dd6
Jose Rodriguez <shammy@localhost.localdomain>
2017-03-07 19:27:51 -0600

    merge before pull request

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170307220602) do
 
   create_table "categories", force: :cascade do |t|

9db8caf1f623846b03ba15c8aaeea36d1aae8fb8
Juan Enrique Segebre Zaghmout <juansegebre@gmail.com>
2017-05-05 01:04:49 -0600

    Changed previous migration create_variables; removed: unit_cost; added: unit_cost_mano

--- db/migrate/20170222152730_create_variables.rb
+++ db/migrate/20170222152730_create_variables.rb
@@ -2,7 +2,7 @@ class CreateVariables < ActiveRecord::Migration[5.0]
   def change
     create_table :variables do |t|
       t.string :measurement_unit
-      t.decimal :unit_cost
+      t.decimal :unit_cost_mano
       t.string :name
       t.references :category, foreign_key: true
 

ffb5c684ea49e496f5f21bdc24f983d6eafd6460
Juan Enrique Segebre Zaghmout <juansegebre@gmail.com>
2017-05-09 06:30:44 -0600

    changed database migration create valorizations and added unit_cost_total

--- db/migrate/20170222162859_create_valorizations.rb
+++ db/migrate/20170222162859_create_valorizations.rb
@@ -7,6 +7,7 @@ class CreateValorizations < ActiveRecord::Migration[5.0]
       t.decimal :amount
       t.decimal :unit_cost_mano
       t.decimal :unit_cost_insumo
+      t.decimal :unit_cost_total
       t.decimal :cost_mano
       t.decimal :cost_insumo
 

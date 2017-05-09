77148b701a51367247e6c9d25205df07ef744271
Juan Enrique Segebre Zaghmout <juansegebre@gmail.com>
2017-05-08 19:51:53 -0600

    changed unit_cost-->unit_cost_mano -line 60- in app\models\lot.rb

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -57,7 +57,7 @@ class Lot < ApplicationRecord
     value = self.valorizations.find_by(variable: var, assigned_at: Date.today)
     hash = { name: var.name, unit_cost_mano: var.unit_cost_mano, amount: 0, subtotal: 0 }
     unless value.nil?
-      hash.merge!({ unit_cost: value.unit_cost, amount: value.amount, subtotal: value.subtotal })
+      hash.merge!({ unit_cost_mano: value.unit_cost_mano, amount: value.amount, subtotal: value.subtotal })
     end
 
     hash

d8a01025efd0ec16a03450c0151544a24c7abc5e
Juan Enrique Segebre Zaghmout <juansegebre@gmail.com>
2017-05-23 23:36:12 -0600

    Checked

--- db/migrate/20170523220059_add_cosechavariables_to_valorizations.rb
+++ db/migrate/20170523220059_add_cosechavariables_to_valorizations.rb
@@ -1,12 +1,12 @@
 class AddCosechavariablesToValorizations < ActiveRecord::Migration[5.0]
   def change
-    add_column :valorizations, :metric_tons, :string
-    add_column :valorizations, :clusters, :string
-    add_column :valorizations, :bags, :string
-    add_column :valorizations, :unit_cost_ton, :string
-    add_column :valorizations, :clusters_per_amount, :string
-    add_column :valorizations, :plants, :string
-    add_column :valorizations, :bags_per_amount, :string
-    add_column :valorizations, :cluster_weight, :string
+    add_column :valorizations, :metric_tons, :decimal
+    add_column :valorizations, :clusters, :decimal
+    add_column :valorizations, :bags, :decimal
+    add_column :valorizations, :unit_cost_ton, :decimal
+    add_column :valorizations, :clusters_per_amount, :decimal
+    add_column :valorizations, :plants, :decimal
+    add_column :valorizations, :bags_per_amount, :decimal
+    add_column :valorizations, :cluster_weight, :decimal
   end
 end
